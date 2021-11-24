a0a0c59c24a820259298b3a0dfdd93e7e11ed35d
Silvergriffon <silverchampion@silverchampion.com>
2021-11-24 14:28:13 -0500

    initial commit by SolastaMod

---
+++ SolastaFaceUnlock/Settings.cs
@@ -0,0 +1,15 @@
+using UnityModManagerNet;
+
+namespace SolastaFaceUnlock
+{
+    public class Core
+    {
+
+    }
+    
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool toggleTest1;
+        public bool toggleTest2;
+    }
+}
